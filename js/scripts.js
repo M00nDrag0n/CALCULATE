8ed4fdb5beae1480c6ee39c139c0b87fd0748268
Anise Lester and Jennifer Batara <jennifer@>
2019-08-12 13:57:53 -0700

    Adds subtract, multiply, and remainder functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,18 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var subtract = function(number1, number2){
+  return number1 - number2;
+}
+
+var multiply = function(num1, num2){
+  return num1 * num2;
+}
+
+var remainder = function(num1, num2){
+  return num1/num2;
+}
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt(">>>>>>>>>>>>>USE YOUR KEYBOARD TO SUBMIT ANOTHER NUMBER."));
 
